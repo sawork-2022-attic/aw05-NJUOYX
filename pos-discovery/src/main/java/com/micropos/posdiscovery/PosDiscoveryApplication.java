41bf964c2d21112fdb70d9d638ec6509893af4ae
NJUOYX <49577453+NJUOYX@users.noreply.github.com>
2022-05-18 21:40:00 +0800

    使用eureka初始化pos-discovery

---
+++ pos-discovery/src/main/java/com/micropos/posdiscovery/PosDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.micropos.posdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosDiscoveryApplication.class, args);
+	}
+
+}
